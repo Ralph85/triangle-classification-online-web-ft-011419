9db9eb7e22037fc4da14e66cabfa15ad75a791b6
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-02-03 02:50:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
   end
 
   def kind()
-    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+    if (@side_1 <= 4) || (@side_2 <= 10) || (@side_3 <= -5)
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2)|| (@side_2+@side_3 <= @side_1)
       raise TriangleError
